8d9645d65e7b5628c79fce1b2d9ecebaab9dd220
omar_swidan <oswidan97@gmail.com>
2019-03-25 00:16:59 +0200

    added the server class with some of the logic but with an unresolved bug

--- Server.cpp
+++ Server.cpp
@@ -2,4 +2,41 @@
 // Created by omar_swidan on 24/03/19.
 //
 
+
+#define __USE_XOPEN2K
+#define _POSIX_C_SOURCE 200112L
+#include <strings.h>
+#include <sys/types.h>
+#include <netdb.h>
+
 #include "Server.h"
+using  std::cout;
+
+void Server::start() {
+
+    //preparing to launch
+    addrinfo hints,*results;
+    int status;
+    //make sure struct hints is empty
+    memset(&hints,0,sizeof hints);
+
+    hints.ai_family=AF_INET;            //IPv4
+    hints.ai_socktype=SOCK_STREAM;      //TCP
+    hints.ai_flags=AI_PASSIVE;          //fill in my ip for me
+
+
+    status = getaddrinfo(nullptr,"80",&hints,&results);
+    if(status != 0 )            //status is nonzero in case of error.
+        cout<<"error";
+    cout<<results;
+
+    //creating the socket
+    int socketRes=socket(AF_INET,SOCK_STREAM,0);
+    if(socketRes<0)
+        cout<<"error";
+
+
+
+    //bind(socketRes,INADDR_ANY,)//INADDR_ANY is the current host ip address
+
+}
--- Server.h
+++ Server.h
@@ -6,8 +6,17 @@
 #define SOCKET_PROGRAMMING_SERVER_H
 
 
+#include <sys/socket.h>
+#include <iostream>
+#include <error.h>
+#include <netinet/in.h>
+#include <cstring>
 class Server {
 
+     struct addrinfo hints;
+
+public:
+    void start();
 };
 
 
