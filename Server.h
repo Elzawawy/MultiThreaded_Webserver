e022128e9eb8bfdac8b6ae91c0d215d0750d2908
Elzawawy <amr22.elzawawy@gmail.com>
2019-04-13 23:17:43 +0200

    Server .cpp and .h files commented and cleaned into multiple modular functions.

--- Server.h
+++ Server.h
@@ -11,12 +11,33 @@
 #include <error.h>
 #include <netinet/in.h>
 #include <cstring>
+#include <vector>
+
+
 class Server {
-
-
 
 public:
     void start();
+
+private:
+    void show_welcome_message(struct sockaddr_storage *their_addr);
+
+    void *client_handler(struct sockaddr_storage *their_addr, int client_socket);
+
+    std::string receive_request(int client_socket);
+
+    void send_response(int client_socket, std::string *response);
+
+    std::string *getResponse(std::string request);
+
+    std::vector<std::string> *parse_string(std::string input);
+
+    std::string *readFileIfExists(std::string fileName);
+
+    void writeToFile(const std::string &body, const std::string &fileName);
+
+    std::string *get_data(std::string message);
+
 };
 
 
